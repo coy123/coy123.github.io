e1c604283baf54b3a5f52e6d9f94ab8d1722a987
Can <canyil@gmail.com>
2026-02-01 22:24:31 +0100

    comment out google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next'
-import Script from 'next/script'
+// import Script from 'next/script'
 import Navigation from '@/components/Navigation'
 import Footer from '@/components/Footer'
 import './globals.css'
@@ -30,15 +30,15 @@ export default function RootLayout({
     return (
         <html lang="it">
         <head>
-            <meta name="google-adsense-account" content="ca-pub-9161475235821616"/>
+            {/* <meta name="google-adsense-account" content="ca-pub-9161475235821616"/> */}
         </head>
         <body>
-        <Script
+        {/* <Script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9161475235821616"
             crossOrigin="anonymous"
             strategy="afterInteractive"
-        />
+        /> */}
         <div className="min-h-screen bg-gray-800 flex flex-col">
             <Navigation/>
             <div className="flex-1 py-4 sm:py-8 px-4 sm:px-6 lg:px-8">
